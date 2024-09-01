082af63d5be2b25a8a95d7a31e5c423e288be8f6
ChiashengLin <stacylin.sunny@gmail.com>
2024-09-01 18:33:08 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/week4_20240901/',
+  base: '/week4_20240901_V3/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
